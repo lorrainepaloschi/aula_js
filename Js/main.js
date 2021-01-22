68f238803ca0bf2464c511ee804153390697fc0c
Lorraine Vieira <lorraine.paloschi@gmail.com>
2021-01-22 02:43:13 -0300

    aula js

---
+++ Js/main.js
@@ -0,0 +1,134 @@
+function botao() {
+
+    document.getElementById("agradecimento").innerHTML = "Obrigada por Clicar";
+    //alert("Obg por clicar");
+    //console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar() {
+    window.open("https://twitter.com");
+    //window.location.href= "https://twitter.com";
+}
+
+
+function trocar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse";
+    elemento.innerHTML = "Obrigada por passar o mouse";
+   // alert("trocar texto");
+}
+
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function load() {
+    alert("Pagina carregada");
+    
+}
+
+
+/* function soma(n1,n2){
+    return n1+n2;
+} */
+
+/* function validaIdade(idade) {
+    var validar;
+    if(idade>=18){
+        validar = true
+    }
+    else{
+        validar = false
+    }
+    return validar;
+}
+
+
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade)); */
+ 
+/* function setReplace(frase, nome, novo_nome) {
+    frase.replace(nome, novo_nome);
+}
+
+alert(soma(5,10)); */
+
+
+//alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+/* var d = new Date();
+alert("hoje" d.getMonth()+1);
+
+alert(d.getDate());
+alert(d.getMinutes()); */
+
+
+
+/* var count;
+for(count=0; count<5; count++){
+    alert(count);
+} */
+
+
+
+
+/* var count= prompt ("Insira um valor");
+while (count <= 5) {
+    console.log(count);
+    count++;
+}; */
+
+
+
+
+/* var idade = prompt("Qual a sua idade?");
+
+if(idade>=18){
+    alert("maior de idade");
+}
+else{
+    alert("menor de idade");
+}
+ */
+
+
+
+/* var fruta = [{nome: "maça", cor: "vermelha"},{nome: "uva", cor: "roxa"} ]
+console.log(fruta[1].nome);
+alert(fruta[1].cor); 
+ */
+
+
+
+
+/* var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor); */
+
+
+
+
+/* var lista = ["maça", "pêra", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista);
+
+console.log(lista.length); */
+
+
+
+
+/* var nome = "Lorraine Paloschi";
+var idade = 21;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome +" tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil"));
+ */
+
